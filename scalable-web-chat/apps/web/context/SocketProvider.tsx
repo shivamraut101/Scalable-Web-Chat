b32b6ce62ea0abbc7b2b80ff9c9f110d9e378a5d
shivamraut101 <shivamkumarraut12@gmail.com>
2023-12-27 15:27:07 +0530

    commented ngrok part

--- scalable-web-chat/apps/web/context/SocketProvider.tsx
+++ scalable-web-chat/apps/web/context/SocketProvider.tsx
@@ -46,17 +46,17 @@ interface ISocketContext {
 
     useEffect(()=>{
         const _socket = io("http://192.168.1.2:8000");
-        const __socket = io("https://mint-parrot-namely.ngrok-free.app:8000");
+        // const __socket = io("https://mint-parrot-namely.ngrok-free.app:8000");
         _socket.on('message', onMessageRec);
-        __socket.on('message', onMessageRec);
+        // __socket.on('message', onMessageRec);
         setSocket(_socket);
-        setSocketNg(__socket);
+        // setSocketNg(__socket);
 
         return ()=>{
             _socket.disconnect();
-            __socket.disconnect();
+            // __socket.disconnect();
             _socket.off('message', onMessageRec);
-            __socket.off('message', onMessageRec);
+            // __socket.off('message', onMessageRec);
             setSocket(undefined);
             setSocketNg(undefined);
         };
